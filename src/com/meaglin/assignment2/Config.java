a2c6acebe03b2b72b1bcf55f339202f1eb0f947a
lunctis-viribus <a@a.a>
2017-11-24 17:45:14 +0100

    Added javadocs

--- src/com/meaglin/assignment2/Config.java
+++ src/com/meaglin/assignment2/Config.java
@@ -1,19 +1,25 @@
 package com.meaglin.assignment2;
 
+/**
+ * Represents a configuration for the host on which a node runs.
+ */
 public class Config {
 
+    String host = "127.0.0.1";          // Host IP.
+    String remotehost = "127.0.0.1";    // Remote host IP.
+    String port = "1099";               // Port number.
+    int nodeCount = 6;                  // Total number of nodes.
+    boolean isServer = false;           // Boolean indicating whether this is a server configuration or not.
 
-    String host = "127.0.0.1";
-    String remotehost = "127.0.0.1";
-
-
-    String port = "1099";
-
-    int nodeCount = 6;
-    boolean isServer = false;
-
+    /**
+     * Returns the URL for a given node ID.
+     * @param nodeId    Unique ID of the node.
+     * @return          The complete URL of the node is returned as a string.
+     */
     public String getUrl(int nodeId) {
-        String host = this.host;
+        String host = this.host;        // Initialize host address of this configuration as variable.
+
+        // The host is changed to the address of the remote host if the given node is not running on localhost.
         if (isServer && (nodeId >= (nodeCount/2))) {
             host = remotehost;
         } else if (!isServer && (nodeId < (nodeCount/2))) {
--- src/com/meaglin/assignment2/NodeRunner.java
+++ src/com/meaglin/assignment2/NodeRunner.java
@@ -5,21 +5,40 @@ import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 
+/**
+ * Runs a distributed node.
+ */
 public class NodeRunner implements Runnable {
 
-
     DA_Peterson iface;
-    public NodeRunner(int nodeId, int nodeCount, Config config) throws RemoteException {
-        iface = new DA_Peterson(nodeId, nodeCount, config);
+
+    /**
+     * Constructs a NodeRunner by creating a new interface and binding
+     * the URL of this interface to the local registry.
+     * @param nodeId            Unique ID of the node.
+     * @param nodeCount         Total number of nodes.
+     * @param config            Server configuration.
+     */
+    public NodeRunner(int nodeId, int nodeCount, Config config) {
+        try {
+            iface = new DA_Peterson(nodeId, nodeCount, config);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
         try {
             Naming.bind(iface.getNodeUrl(), (DA_Peterson_RMI) iface);
         } catch (AlreadyBoundException e) {
             e.printStackTrace();
         } catch (MalformedURLException e) {
             e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
         }
     }
 
+    /**
+     * The run method starts the local interface of our distributed algorithm.
+     */
     @Override
     public void run() {
         iface.start();
