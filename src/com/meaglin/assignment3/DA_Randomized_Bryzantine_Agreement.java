cb2a20e0775bf627a5cdc042a2be36f7056e1ce1
Joseph Verburg <josephverburg@gmail.com>
2017-12-01 17:51:39 +0100

    Working implementation

--- src/com/meaglin/assignment3/DA_Randomized_Bryzantine_Agreement.java
+++ src/com/meaglin/assignment3/DA_Randomized_Bryzantine_Agreement.java
@@ -3,6 +3,8 @@ package com.meaglin.assignment3;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class DA_Randomized_Bryzantine_Agreement extends UnicastRemoteObject implements Runnable, DA_Randomized_Bryzantine_Agreement_RMI {
@@ -16,7 +18,7 @@ public class DA_Randomized_Bryzantine_Agreement extends UnicastRemoteObject impl
 
     Node[] nodes;
 
-    int value;
+    volatile int value;
 
     boolean decided;
 
@@ -25,6 +27,9 @@ public class DA_Randomized_Bryzantine_Agreement extends UnicastRemoteObject impl
         id = nodeId;
         this.nodeCount = nodes.length;
         this.nodes = nodes;
+
+
+        bufferedMessages = new ArrayList<>();
 
         try {
             Naming.bind("rmi://127.0.0.1:1099/node_" + id, this);
@@ -37,10 +42,14 @@ public class DA_Randomized_Bryzantine_Agreement extends UnicastRemoteObject impl
         start();
     }
 
-    public synchronized void start() {
-        round = new Round(0, nodeCount);
-        value = (int) (Math.random() * 2);
-        decided = false;
+    public void start() {
+        synchronized (this) {
+            round = new Round(0, nodeCount);
+//            value = 1;
+            value = (int) Math.floor(Math.random() * 2);
+            decided = false;
+            System.out.println("[Node " + id + "] starting round 0 with value " + value);
+        }
         broadcast(value);
     }
     /*
@@ -98,68 +107,106 @@ public class DA_Randomized_Bryzantine_Agreement extends UnicastRemoteObject impl
 
      */
     @Override
-    public synchronized void receive(Message message) throws RemoteException {
-        if (decided) {
-            return;
-        }
+    public void receive(Message message) throws RemoteException {
+        System.out.println("[Node " + id + "] receiving " + message.value);
+        int nextBroadcast;
         try {
             Thread.sleep((long) (Math.random() * 50));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        if (!isValidMessage(message)) {
-            return;
-        }
-        // Discard messages of uninteresting rounds (previous rounds)
-        if (message.round < round.id) {
-            return;
-        }
-        if (message.round > round.id) {
-            bufferedMessages.add(message);
-        }
-        round.processMessage(message);
-        if (round.phase == Round.Phase.NOTIFICATION &&
-                round.notificationThresholdReached()) {
-            round.phase = Round.Phase.PROPOSAL;
-            broadcast(round.notificationConsensus());
-        } else if (round.phase == Round.Phase.PROPOSAL &&
-                round.proposalThresholdReached()) {
 
-            round.phase = Round.Phase.DECISION;
-            int lowConsensus = round.proposalLowConsensus();
-            if (lowConsensus == 0 || lowConsensus == 1) {
-                value = lowConsensus;
-                int decidedConsensus = round.proposalDecidedConsensus();
-                if (decidedConsensus == 0 || decidedConsensus == 1) {
-                    decided = true;
-                    value = decidedConsensus;
-                    System.out.println("[Node " + id + "] decided " + value);
-                    return;
+        nextBroadcast = processMessage(message);
+        if (nextBroadcast != -1) {
+            broadcast(nextBroadcast);
+        }
+        checkBuffer();
+    }
+
+    private int processMessage(Message message) {
+        synchronized(this) {
+            if (decided) {
+                return -1;
+            }
+            if (!isValidMessage(message)) {
+                return -1;
+            }
+            // Discard messages of uninteresting rounds (previous rounds)
+            if (message.round < round.id) {
+                return -1;
+            }
+            if (message.round > round.id) {
+                bufferedMessages.add(message);
+                return -1;
+            }
+            System.out.println("[Node " + id + "] processing");
+            round.processMessage(message);
+            if (round.phase == Round.Phase.NOTIFICATION &&
+                    round.notificationThresholdReached()) {
+                round.phase = Round.Phase.PROPOSAL;
+                return round.notificationConsensus();
+            } else if (round.phase == Round.Phase.PROPOSAL &&
+                    round.proposalThresholdReached()) {
+
+                round.phase = Round.Phase.DECISION;
+                int lowConsensus = round.proposalLowConsensus();
+                if (lowConsensus == 0 || lowConsensus == 1) {
+                    value = lowConsensus;
+                    int decidedConsensus = round.proposalDecidedConsensus();
+                    if (decidedConsensus == 0 || decidedConsensus == 1) {
+                        decided = true;
+                        value = decidedConsensus;
+                        System.out.println("[Node " + id + "] decided " + value);
+                        return -1;
+                    }
+                } else {
+                    value = (int) Math.floor(Math.random() * 2);
+                }
+                round = new Round(round.id + 1, nodeCount);
+
+                System.out.println("[Node " + id + "] starting round " + round.id);
+
+                return value;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    public void checkBuffer() throws RemoteException {
+        Message msg = null;
+        synchronized (this) {
+            Iterator<Message> it = bufferedMessages.iterator();
+            while(it.hasNext()) {
+                Message next = it.next();
+                if (next.round <= round.id) {
+                    it.remove();
+                    msg = next;
+                    break;
                 }
             }
-            round = new Round(round.id + 1, nodeCount);
-            broadcast(value);
+        }
+        if (msg != null) {
+            receive(msg);
         }
     }
 
     public void broadcast(int value) {
         Message message = null;
-        if (!round.hasNotified) {
-            message = new Message(id, 1, round.id, value);
-            round.hasNotified = true;
-            round.processMessage(message);
-        } else if (!round.hasProposed) {
-            message = new Message(id, 2, round.id, value);
-            round.hasProposed = true;
-            round.processMessage(message);
+        synchronized(this) {
+            if (!round.hasNotified) {
+                message = new Message(id, 1, round.id, value);
+                round.hasNotified = true;
+            } else if (!round.hasProposed) {
+                message = new Message(id, 2, round.id, value);
+                round.hasProposed = true;
+            }
         }
         if (message == null) {
             return;
         }
+        System.out.println("[Node " + id + "] broadcasting " + message.value);
         for(int i = 0; i < nodeCount; i++) {
-            if (i == id) {
-                continue;
-            }
             Node node = nodes[i];
             node.sendMessage(message);
         }
@@ -169,7 +216,7 @@ public class DA_Randomized_Bryzantine_Agreement extends UnicastRemoteObject impl
         return (message.round >= 0 &&
                 message.nodeId >= 0 &&
                 message.nodeId < nodeCount &&
-                (message.value == 0 || message.value == 1) &&
+                (message.value == 0 || message.value == 1 || message.value == -2) &&
                 (message.type == 1 || message.type == 2)
         );
     }
